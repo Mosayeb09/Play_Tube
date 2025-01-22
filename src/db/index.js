eee5d35bb9c19f0538a938739cbc563549dcbf48
Mosayeb09 <mosayebhossain09@gmail.com>
2025-01-23 01:42:31 +0600

    created external db connection

--- src/db/index.js
+++ src/db/index.js
@@ -6,9 +6,15 @@ import { DB_NAME } from './constants';
 const connectDB = async () =>{
 
     try {
+         const connectionInstance =await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+         console.log(`"/n MONGODB CONNECTION SUCCESSFULLY !! DB HOST:" ${connectionInstance.connection.host}`);
         
     } catch (error) {
+        console.error("MONGODB CONNECTION ERROR",error);
+        process.exit(1);
         
     }
 
 }
+
+export default connectDB;
