be53ac101c19776d8f49f62e4e33cbd24313dc72
Mosayeb09 <mosayebhossain09@gmail.com>
2025-01-25 01:26:45 +0600

    create cloudinary for file upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,26 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
+
+ const  uploadOnCloudinary = async (localFilePath) => {
+
+    try {
+        if (!localFilePath) {
+            return null;
+        }
+        // upload file to cloudinary
+       const response = await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+        // file has been uploaded successfully
+        console.log("File uploaded successfully",response.url);
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove file , witch was temporarily uploaded in server
+        return null
+    }
+
+ }
+
+ export {uploadOnCloudinary}
+    
