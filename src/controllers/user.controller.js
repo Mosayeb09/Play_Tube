2a04ea52244c6ec9da3493b9609c7a172b91a04e
Mosayeb09 <mosayebhossain09@gmail.com>
2025-02-07 01:10:59 +0600

    user details managment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -243,7 +243,8 @@ const getCurrentUser = asyncHandler(async(req,res) =>{
   .json(new ApiResponse(200,req.user,"Current user fetched successfully"))
 })
 
-  const updateAccountDetails = asyncHandler(async(req,res) =>{
+
+const updateAccountDetails = asyncHandler(async(req,res) =>{
 
     const {fullName,email}= req.body
 
@@ -269,6 +270,71 @@ const getCurrentUser = asyncHandler(async(req,res) =>{
 
 
 
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+
+   const avatarLocalPath = req.file?.path
+
+   if(!avatarLocalPath){
+    throw new ApiError(400,"Avatar required")
+   }
+
+   const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+   if(!avatar.url){
+    throw new ApiError(400,"Something went wrong while uploading avatar")
+   }
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set:{
+          avatar:avatar.url
+        }
+      },
+      {
+        new:true
+      }
+    ).select("-password")
+
+    return res.status(200)
+    .json(new ApiResponse(200,user,"Avatar updated successfully"))
+})  
+
+const updateUserCoverImage = asyncHandler(async(req,res)=>{
+
+  const coverImageLocalPath = req.file?.path
+
+  if(!coverImageLocalPath){
+   throw new ApiError(400,"Cover image required")
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if(!coverImage.url){
+   throw new ApiError(400,"Something went wrong while uploading cover image")
+  }
+
+    const user =await User.findByIdAndUpdate(
+     req.user._id,
+     {
+       $set:{
+         avatar:coverImage.url
+       }
+     },
+     {
+       new:true
+     }
+   ).select("-password")
+
+   return res.status(200)
+   .json(new ApiResponse(200,user,"Cover image updated successfully"))
+}) 
+
+
+
+
+
+
 export {
    registerUser,
    loginUser,
@@ -276,6 +342,8 @@ export {
    refreshAccessToken,
    changeCurrentPassword,
    getCurrentUser,
-   updateAccountDetails
+   updateAccountDetails,
+   updateUserAvatar,
+   updateUserCoverImage
 
  };
